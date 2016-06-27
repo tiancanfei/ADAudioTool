0839c9f186792df2b3371d438cba93678fb69bc8
安德航 <bjwltiankong@163.com>
2016-06-27 17:39:11 +0800

    Initial Commit

---
+++ SystemSoundsPlay/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SystemSoundsPlay
+//
+//  Created by andehang on 16/6/27.
+//  Copyright © 2016年 andehang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SystemSoundsPlay/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  SystemSoundsPlay
+//
+//  Created by andehang on 16/6/27.
+//  Copyright © 2016年 andehang. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
